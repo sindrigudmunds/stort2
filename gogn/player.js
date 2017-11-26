954f2023e59daa0e90fc573fa5c0b98ef84f002f
hornheidars <hornheidars@gmail.com>
2017-11-26 17:58:24 +0000

    DOM element

--- gogn/player.js
+++ gogn/player.js
@@ -1,3 +1,4 @@
+/*
 var video = document.querySelector('video');
 var button = document.querySelector('button');
 button.addEventListener('click', function () {
@@ -11,3 +12,43 @@ button.addEventListener('click', function () {
     button.appendChild(document.createTextNode('Play'));
   }
 });
+*/
+
+
+// Búa til DOM elementin
+var container = document.crateElement('div');
+container.className = 'container';
+document.body.appendChild(container);
+
+var videoplayer = document.crateElement('div');
+videoplayer.className = 'videoplayer';
+container.appendChild(videoplayer);
+
+var video = document.crateElement('div');
+video.className = 'video';
+videoplayer.appendChild(video);
+
+var nest_play = document.createElement('button');
+nest_play.className = 'nest_play';
+video.appendChild(nest_play);
+
+var toolbox = document.crateElement('div');
+toolbox.className = 'toolbox';
+videoplayer.appendChild(toolbox);
+
+var back = document.createElement('button');
+back.className = 'back';
+var play = document.createElement('button');
+play.className = 'play';
+var mute = document.createElement('button');
+mute.className = 'mute';
+var fullscreen = document.createElement('button');
+fullscreen.className = 'fullscreen';
+var forward = document.createElement('button');
+forward.className = 'forward';
+
+toolbox.appendChild(back);
+toolbox.appendChild(play);
+toolbox.appendChild(mute);
+toolbox.appendChild(fullscreen);
+toolbox.appendChild(forward);
