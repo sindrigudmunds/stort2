f5754e9debbe7de5dfca1732398becfcbc6647ed
sindrigudmunds <sthg11@hi.is>
2017-11-26 15:03:00 +0000

    Commitblascriptseitthvað

--- gogn/script.js
+++ gogn/script.js
@@ -12,8 +12,44 @@ function createSite(){
 }
 
 function createVideoElement(id, title, created, duration, poster, video) {
-  console.log(id);
-  
+  var card = document.createElement("div");
+  card.classList.add("card");
+
+  var card__img = document.createElement("div");
+  card__img.classList.add("card__img");
+
+  var image = document.createElement("img");
+  image.setAttribute("src", poster);
+
+  var time = document.createElement("div");
+  time.classList.add("card__time");
+
+  var min = Math.floor(duration/60);
+  var sek = duration - min*60;
+  var ftime = document.createTextNode(min +":"+sek);
+
+  var card__content = document.createElement("div");
+  card__content.classList.add("card__content");
+
+  var card__title = document.createElement("div");
+  card__title.classList.add("card__title");
+  var card__title__title = document.createElement("h3");
+  card__title__title.appendChild(document.createTextNode(title));
+  card__title.appendChild(card__title__title);
+
+  var card__text = document.createElement("div");
+  card__text.classList.add("card__text");
+  card__text.appendChild(document.createTextNode("Fyrir " + created + " síðan."));
+
+  time.appendChild(ftime);
+  card__img.appendChild(image);
+  card__img.appendChild(time);
+  card__content.appendChild(card__title);
+  card__content.appendChild(card__text);
+  card.appendChild(card__img);
+  card.appendChild(card__content);
+  document.querySelector(".category").appendChild(card);
+  //<img src="videos/bunny.png">
 }
 
 function createPartition() {
@@ -50,6 +86,7 @@ function pullData() {
           var response = JSON.parse(request.responseText);
           //console.log(response.videos[1]);
           for (var i = 0; i < response.categories.length; i++) {
+            console.log(response.categories[0].videos[1]);
             createCategory(response.categories[i].title);
             for (var j = 0; j < response.categories[i].videos.length; j++) {
               for (var k = 0; k < response.videos.length; k++) {
